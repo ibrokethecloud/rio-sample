25fe55a663fb66a7dfbbd7a100acfa75ec834516
Gaurav Mehta <gaurav@rancher.com>
2020-08-03 13:29:21 +1000

    Updated sample app to fetch node name and evaluate clusterName for now

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/ibrokethecloud/rio-sample
 
 go 1.13
+
+require (
+	github.com/googleapis/gnostic v0.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/apimachinery v0.18.0
+	k8s.io/client-go v0.18.0
+	sigs.k8s.io/structured-merge-diff v0.0.0-20190525122527-15d366b2352e // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"golang.org/x/net/context"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +23,65 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		bgColor = "powderblue"
 	}
 
-	fmt.Fprintf(w, "<html><body style=\"background-color:%s;\"><h1>Rio Demo</h1><p>Sample application to show case Rio code workflows</p></body></html>", bgColor)
+	nodeName, clusterName, err := fetchDetails()
+	if err != nil {
+		log.Println(err)
+		nodeName = "default"
+		clusterName = "kubernetes"
+	}
+
+	fmt.Fprintf(w, "<html><body style=\"background-color:%s;\"><h1>Demo App</h1><p>Demo app running on node %s and cluster %s </p></body></html>", bgColor, nodeName, clusterName)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func fetchDetails() (nodeName string, clusterName string, err error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nodeName, clusterName, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nodeName, clusterName, err
+	}
+
+	namespace, err := fetchNamespace()
+	if err != nil {
+		return nodeName, clusterName, err
+	}
+
+	podName, err := os.Hostname()
+	if err != nil {
+		return nodeName, clusterName, err
+	}
+
+	pod, err := clientset.CoreV1().Pods(namespace).Get(context.Background(), podName, v1.GetOptions{})
+	if err != nil {
+		return nodeName, clusterName, err
+	}
+
+	nodeName = pod.Spec.NodeName
+
+	// Using an assumption the node name will be of format prefix-ClusterName-role-Count
+	nodeArray := strings.Split(nodeName, "-")
+	if len(nodeArray) >= 4 {
+		clusterName = strings.Join(nodeArray[1:len(nodeArray)-2], "-")
+	} else {
+		clusterName = "kubernetes"
+	}
+
+	return nodeName, clusterName, err
+}
+
+func fetchNamespace() (namespace string, err error) {
+	nsContent, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		return "", err
+	}
+
+	namespace = string(nsContent)
+	return namespace, err
 }
